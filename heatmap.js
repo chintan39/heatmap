3dd7f25389ee2118bf3e2b5f2d30823609841de5
chintan <hi@chintan.pw>
2013-10-20 23:43:26 +0530

    IMprovements in Heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -1,8 +1,14 @@
+var code = document.getElementById("heatmap").getAttribute("data-code");
+// alert(code);
+// alert($( window ).width());
+// alert($( window ).height());
+
+var allowed_el = ['A','LI','SPAN'];
+
 (function($){
     $.fn.extend({
         getFullPath: function()
         {
-            alert(this.length);
             function traverseUp(el)
             {
                 var result = el.tagName + ':eq(' + $(el).index() + ')';
@@ -13,16 +19,32 @@
                 }                
                 return result;
             };
-            return this.length > 0 ? traverseUp(this[0]) : '';
+            if($.inArray(this[0].tagName,allowed_el)!==-1)
+            {
+                return this.length > 0 ? traverseUp(this[0]) : '';
+            }
+            else
+            {
+                return 0;
+            }
+            
         }
     });
 })(jQuery);
 
+
 $('*').click(function(e)
 {
-    alert(e.target);
-    alert('Clicked');
     var fullPath = $(this).getFullPath();
-        // alert(fullPath);
+    if(fullPath!=0)
+    {
+        alert('Send to Server');
         console.log(fullPath);
+        var parentOffset = $(this).offset(); 
+        var relX = e.pageX - parentOffset.left;
+        alert(relX);
+        var relY = e.pageY - parentOffset.top;
+        alert(relY);
+    }
+    e.stopPropagation();
 });
